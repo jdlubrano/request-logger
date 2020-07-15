0cf01b9f77dbca47fe1ee9d431faa2b6f88ddfb7
Joel Lubrano <joel.lubrano@stitchfix.com>
2020-07-15 13:52:00 -0400

    Add GET endpoint to render images

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const path = require('path');
+
 require('dotenv').config()
+
+const random = require('lodash.random');
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+const IMAGES = [
+  'pooh.png',
+  'rexcellent.gif'
+];
 
 app.use(cors());
 app.use(express.json());
@@ -30,6 +39,26 @@ app.post('/v2/logs', (req, res) => {
   `);
 
   res.status(200).send();
+});
+
+app.get('/images/:clientId/:imageId?', (req, res) => {
+  console.log(`
+    INCOMING REQUEST GET /images
+    HEADERS: ${JSON.stringify(req.headers)}
+    QUERY PARAMS: ${JSON.stringify(req.query)}
+    ROUTE PARAMS: ${JSON.stringify(req.params)}
+  `);
+
+  options = {
+    root: path.join(__dirname, 'public')
+  }
+
+  imageIndex = random(0, IMAGES.length - 1, false);
+  image = IMAGES[imageIndex];
+
+  console.log(`Rendering ${image}`);
+
+  res.sendFile(image, options);
 });
 
 app.listen(port, () => console.log(`request-logger listening on port ${port}!`));
